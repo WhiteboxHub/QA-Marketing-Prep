3417a47398fe629352b7fdbc7b8b0fef3cd563b7
VishnuPriya <valleri.vishnupriya@gmail.com>
2015-11-10 22:07:51 -0800

    Pushing few loop Programs

---
+++ JavaAssignment/src/com/innovapath/programs/loops/Amstrong.java
@@ -0,0 +1,21 @@
+package practice2;
+
+public class Amstrong {
+
+	public static void main(String[] args) {
+		int r,n=153;
+		int sum=0;
+		int temp=n;
+		while(n>0){
+			r=n%10;
+			sum=sum+(r*r*r);
+			n=n/10;
+		}if(temp==sum){
+			System.out.println("This is a amstrong number");
+		}else
+		{
+			System.out.println("This is not a amstrong number");
+		}
+	}
+
+}
---
+++ JavaAssignment/src/com/innovapath/programs/loops/PingPong.java
@@ -0,0 +1,22 @@
+package practice2;
+
+public class PingPong {
+
+	public static void main(String[] args) {
+		int n=25;
+		if(n%3==0 && n%5==0)
+		{
+			System.out.println("PingPong");
+		}else if(n%3==0)
+		{
+			System.out.println("Ping");
+		}else if(n%5==0)
+		{
+			System.out.println("Pong");
+		}else
+		{
+			System.out.println(n);
+		}
+	}
+
+}
