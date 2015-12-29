67116d160fa1174febe490490c8caf93364bdcf6
VishnuPriya <valleri.vishnupriya@gmail.com>
2015-12-28 18:49:34 -0800

    pushing merging two arrays program

---
+++ JavaAssignment/src/com/innovapath/programs/collections/MergeArrays.java
@@ -0,0 +1,17 @@
+package com.innovapath.programs.collections;
+
+
+public class MergeArrays {
+
+	public static void main(String[] args) {
+	
+		String a[]={"a1","a2","a3","a4","a5","a6"};
+		String b[]={"b1","b2","b3","b4","b5","b6"};
+		for(int i=0;i<=a.length-1;i++)
+		{
+			System.out.println(a[i] + "" + b[i]);
+		}
+		
+	}
+
+}
